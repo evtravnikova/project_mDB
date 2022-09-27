08c2c8e323727b4a966e02810b2e9ff54ed8a0ce
Olena Travnikova <elena@travnikova.pro>
2022-09-27 21:09:39 +0200

    Added the ability to push new films with input form and add favorite movies. Ability to delete films from the list without a refreshing site. Long film`s names are reduces.

--- project/js/script.js
+++ project/js/script.js
@@ -1,65 +1,87 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
 
-const movieDB = {
-    movies: [
-        "Тіні забутих предків",
-        "Арго",
-        "Домогосподарки",
-        "Eleven",
-        "Fargo",
-        "Час",
-        "Абетка"
-    ]
-};
+    const movieDB = {
+        movies: [
+            "Тіні забутих предків",
+            "Арго",
+            "Домогосподарки",
+            "Eleven",
+            "Fargo",
+            "Час",
+            "Абетка"
+        ]
+    };
 
-const adv = document.querySelector('.promo__adv');
-const poster = document.querySelector('.promo__bg');
-
-adv.remove();
-document.querySelector('.promo__genre').textContent = 'ДРАМА';
-poster.style.backgroundImage = 'url("img/bg.jpg")';
-createMovieList(movieDB);
-
-
-
-
-
-
-function createMovieList(list) {
     const movieList = document.querySelector('.promo__interactive-list');
-    
-    movieList.innerHTML = '';
-    list.movies.sort();
-    
-    list.movies.forEach((film, i) => {
-        movieList.innerHTML += `
+    const adv = document.querySelector('.promo__adv');
+    const poster = document.querySelector('.promo__bg');
+    const addForm = document.querySelector('form.add');
+    const addInput = addForm.querySelector('.adding__input');
+    const checkbox = addForm.querySelector('[type="checkbox"]');
+
+
+    deleteAdv(adv);
+    changeBackgroundAndGenre();
+    createMovieList(movieDB.movies, movieList);
+
+
+
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`
+            }
+            if (favorite) {
+                console.log('Favorite film is added');
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+        }
+        event.target.reset();
+    })
+
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';
+        sortArr(films);
+
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
                             <li class="promo__interactive-item">${i + 1}. ${film}
                             <div class="delete"></div>
-                        </li>
-    `;
-    });
-}
-
-
-/*
-const movieList = document.querySelectorAll('.promo__interactive-item');
-for (let i = 0; i < movieList.length; i++) {
-    for (let j = 0; j <= i; j++) {
-        movieList[i].innerHTML = movieDB['movies'][j] + '<div class = "delete"></div>'
+                        </li>`;
+        });
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                createMovieList(films, parent);
+            })
+        })
     }
-}
-*/
+
+
+    function deleteAdv(arr) {
+        arr.remove();
+    }
+
+
+    function changeBackgroundAndGenre() {
+        document.querySelector('.promo__genre').textContent = 'ДРАМА';
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    }
+
+    function sortArr(arr) {
+        arr.sort()
+    }
+});
+
